9b055d5407f1e9ef622ecff7c2b2c3182654b1dc
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2026-09-12 20:49:29 -0300

    feat: add header components and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,39 @@
+import Image from 'next/image'
+
+import Header from './_components/header'
+
 import { Button } from './_components/ui/button'
+import { Input } from './_components/ui/input'
+
+import { SearchIcon } from 'lucide-react'
 
 export default function Home() {
   return (
     <div>
-      <h1 className="p-4 py-2 text-2xl font-bold">Home page</h1>
-                        <Button variant="default" size="default">
-                          Click me
-                        </Button>
+      {/*  header  */}
+      <Header />
+      <div className="p-5">
+        {/* Apressentação */}
+        <h2 className="text-xl font-bold">Olá, Guilherme</h2>
+        <p>Segunda-feira, 12 de setembro de 2026.</p>
+        {/* Busca */}
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        {/* Banner */}
+        <div className="relative mt-6 h-37.5 w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+        
+      </div>
     </div>
   )
 }
