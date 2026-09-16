a88f81075cb2a2ec69acd285136042a9ace177b4
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2026-09-15 23:57:50 -0300

    feat: add initial barbershop page

--- app/_components/barbershop-item.tsx
+++ app/_components/barbershop-item.tsx
@@ -1,9 +1,12 @@
+'use client'
+
 import { Barbershop } from '@prisma/client'
 import { Card, CardContent } from './ui/card'
 import Image from 'next/image'
 import { Button } from './ui/button'
 import { Badge } from './ui/badge'
 import { StarIcon } from 'lucide-react'
+import Link from 'next/link'
 
 interface BarbershopItemProps {
   barbershop: Barbershop
@@ -35,7 +38,11 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
         <div className="px-1 py-2">
           <h3 className="truncate font-semibold">{barbershop.name}</h3>
           <p className="truncate text-sm text-gray-400">{barbershop.adress}</p>
-          <Button variant="secondary" className="mt-3 w-full">
+          <Button
+            variant="secondary"
+            className="mt-3 w-full"
+            render={<Link href={`/barbershops/${barbershop.id}`} />}
+          >
             Reservar
           </Button>
         </div>
---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,75 @@
+import { Button } from '@/app/_components/ui/button'
+import { db } from '@/app/_lib/prisma'
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { notFound } from 'next/navigation'
+
+interface BarberShopPageProps {
+  params: {
+    id: string
+  }
+}
+
+const BarbershopPage = async ({ params }: BarberShopPageProps) => {
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+  })
+
+  if (!barbershop) {
+    return notFound()
+  }
+  return (
+    <div>
+      {/* IMAGEM */}
+      <div className="relative h-62.5 w-full">
+        {barbershop?.imageURL && (
+          <Image
+            alt={barbershop.name}
+            src={barbershop.imageURL}
+            fill
+            className="object-cover"
+          />
+        )}
+        <Button
+          render={<Link href={'/'} />}
+          size="icon"
+          variant="outline"
+          className="absolute top-4 left-4"
+        >
+          <ChevronLeftIcon />
+        </Button>
+
+        <Button
+          size="icon"
+          variant="outline"
+          className="absolute top-4 right-4"
+        >
+          <MenuIcon />
+        </Button>
+      </div>
+      {/* INFORMAÇÔES */}
+      <div className="border-b border-solid p-5">
+        <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
+        <div className="mb-2 flex items-center gap-2">
+          <MapPinIcon className="text-primary" size={18} />
+          <p className="text-sm">{barbershop.adress}</p>
+        </div>
+
+        <div className="mb-2 flex items-center gap-2">
+          <StarIcon className="fill-primary text-primary" size={18} />
+          <p className="text-sm">5,0 (584 avaliações)</p>
+        </div>
+      </div>
+      {/* DESCRIÇÂO */}
+      <div className="space-y-3 border-b border-solid p-5">
+        <h2 className="text-xs font-bold text-gray-400 uppercase">Sobre nós</h2>
+        <p className="text-justify text-sm">{barbershop.description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default BarbershopPage
